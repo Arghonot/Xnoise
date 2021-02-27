93768514d5706fa142f4ec9464bff7a6d3bdd45d
Arghonot <lo.rivemale@gmail.com>
2021-02-27 15:44:51 +0000

    Added a cylinder node based on the libnoise.cylinder generator. Removed useless comment.

---
+++ Generator/CylindersNode.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using LibNoise;
+using LibNoise.Generator;
+
+namespace NoiseGraph
+{
+    [CreateNodeMenu("NoiseGraph/Generator/Cylinders")]
+    public class CylindersNode : LibnoiseNode
+    {
+        [Input(ShowBackingValue.Always, ConnectionType.Override, TypeConstraint.Strict)]
+        public double frequency;
+
+        public override object Run()
+        {
+            // if editing the graph -> we stick to current variables
+            if (Application.isEditor && !Application.isPlaying)
+            {
+                return new Cylinders(
+                    this.frequency);
+            }
+
+            return new Cylinders(
+                GetInputValue<double>("frequency", this.frequency));
+        }
+    }
+}
--- Own/BinarizeModule.cs
+++ Own/BinarizeModule.cs
@@ -31,8 +31,6 @@ public class BinarizeModule : SerializableModuleBase
         double val = Modules[0].GetValue(x, y, z);
 
         return (Mathf.Abs((float)val - 1) > Mathf.Abs((float)val + 1)) ? 1d : -1d;
-
-        //return (double)((int)Modules[0].GetValue(x, y, z));
     }
 
     #endregion
