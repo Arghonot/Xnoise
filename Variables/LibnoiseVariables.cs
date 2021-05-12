07fd2fed6e9ba761024b6650bcc4fd713d9af1cb
Arghonot <lo.rivemale@gmail.com>
2021-05-12 22:06:27 +0100

    Added the qualityMode variable in it's own file.

---
+++ Variables/LibnoiseVariables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Graph
+{
+    [Serializable]
+    [StorableType(typeof(LibNoise.QualityMode))]
+    public class QualityModeVariable : VariableStorage<LibNoise.QualityMode> { }
+
+    public partial class GraphVariableStorage : GraphVariableStorageHelper
+    {
+        [SerializeField] public List<QualityModeVariable> QualityModes = new List<QualityModeVariable>();
+    }
+}
